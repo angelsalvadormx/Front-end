84cd413045b812c4e84f1e455704afbf27eb3157
Angel Salvador <02angelsalvador@gmail.com>
2019-03-21 22:13:15 -0600

    calculadora (sin estilos)

---
+++ calculadora/calculadora.js
@@ -0,0 +1,45 @@
+const btns = document.getElementsByClassName('boton');
+const display = document.getElementById('Display');
+var show = '';
+var replace = false;
+var save = '';
+
+function getValue(){
+    let value = this.getAttribute('value');
+    
+    if(replace || 'MR' || 'M+'|| 'C'){
+        show = '';
+        replace = false;
+    }
+    
+    if(value == 'MS'){
+        save = show;
+        value = '';
+    }
+
+    value == 'MR' ? value = save : ''
+    value =  value == 'M+' ? eval(show+"+"+save) : '',
+    value == 'MC' ? save = '' : '';
+    value == 'C' ? value = '' : ''
+
+    if(value == '='){
+        replace = true;
+        show.length > 0 ? Number.isInteger(Number(show[show.length-1])) ? display.value = eval(show) : replace=false : '';
+        return 0;
+    }
+    
+    if(value == '+/-'){
+        value = eval(show*-1);
+        show = '';
+    }
+    
+    if(value == '.'){
+        value = show.indexOf('.') == -1 ? '.' : '';
+    }
+    
+    show += value;
+    
+    display.value = show;
+}
+
+Object.keys(btns).forEach(key => btns[key].addEventListener('click',getValue));
